8378a1fa838ea88ee907a626162795d1d73c706e
RameshriSalian18 <rameshrisalian18@gmail.com>
2025-04-04 15:12:44 +0530

    first push

---
+++ Maven_Practice_Project/src/test/java/day1restAssured/HttpRequest.java
@@ -0,0 +1,25 @@
+package day1restAssured;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class HttpRequest {
+
+	@Test
+	
+	void getUser()
+	{
+		given()
+		
+		.when()
+		  .get("https://reqres.in/api/users?page=2")
+		  
+		.then()
+		 .statusCode(200)
+		 .body("page", equalTo(2))
+		 .log().all();
+	}
+	
+}
---
+++ Maven_Practice_Project/src/test/java/pac1/OrgTest.java
@@ -0,0 +1,17 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createorgTest() {
+		System.out.println("executecontactTest");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modifycontactTest");
+	}
+
+}
